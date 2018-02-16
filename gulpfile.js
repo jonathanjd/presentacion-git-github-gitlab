dd6759e24c90591cad7b5ee42d19d490bf14a1b6
Jonathan Duran <headjd@gmail.com>
2018-02-16 00:41:15 -0400

    Dependencia + Git + Github - Concepto

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const pug = require("gulp-pug");
+
+// Static Server + watching scss/html files
+gulp.task("serve", ['pug'], function() {
+  browserSync.init({
+    server: "./"
+  });
+  gulp.watch('./pug/*.pug', ['pug']);
+  gulp.watch("./*.html").on("change", browserSync.reload);
+});
+
+
+gulp.task('pug', () => {
+
+    gulp.src('./pug/*.pug')
+        .pipe(pug({
+          pretty: true
+        }))
+        .pipe(gulp.dest('./'));
+
+});
